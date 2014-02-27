d1908cbad1c73252d62c2403d6dd498a3696fe9f
dcatcher9 <dcatcher.qu@gmail.com>
2014-02-27 15:48:21 +0800

    comment

--- InfinityGameEngine/InfinityGameEngine/Attribute.h
+++ InfinityGameEngine/InfinityGameEngine/Attribute.h
@@ -4,12 +4,16 @@
 
 namespace InfinityGameEngine
 {
-    // represent ordered atrribute
-    class Attribute
+    // We need to support adding new attribute via config instead of code change.
+    // This flexibility is critical for the extensibility of the engine.
+    // However, we'll have to sacrifice performance or memory for it.
+    
+    // represent static info for ordered atrribute
+    class AttributeSchema
     {
     private:
+        std::string name_;
         double min_value_;
         double max_value_;
-        double cur_value_;
     };
 }
